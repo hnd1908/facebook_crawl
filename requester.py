25dbbb8e6f5772771994c6ff142a4376c2907332
Nguyễn Duy Hoàng <22520467@gm.uit.edu.vn>
2025-06-09 13:34:30 +0700

    update method for get post

--- requester.py
+++ requester.py
@@ -1,6 +1,7 @@
 import requests
 from logger import setup_logger
 import re
+import time
 
 logger = setup_logger(__name__)
 
@@ -12,11 +13,56 @@ class Requester():
         '''
         pageurl = re.sub('www', 'm', pageurl)
         resp = requests.get(pageurl)
-        headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-language': 'en'}
-        headers['cookie'] = '; '.join(['{}={}'.format(cookieid, resp.cookies.get_dict()[
-                                    cookieid]) for cookieid in resp.cookies.get_dict()])
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en'
+        }
+        cookies = resp.cookies.get_dict()
+        headers['cookie'] = '; '.join([f'{k}={v}' for k, v in cookies.items()])
         return headers
+
+    @staticmethod
+    def _get_homepage(pageurl, headers):
+        '''
+        Send a request to get the homepage response
+        '''
+        pageurl = re.sub('/$', '', pageurl)
+        timeout_cnt = 0
+        while True:
+            try:
+                homepage_response = requests.get(pageurl, headers=headers, timeout=3)
+                return homepage_response
+            except:
+                time.sleep(5)
+                timeout_cnt += 1
+                if timeout_cnt > 20:
+                    class homepage_response:
+                        text = 'Sorry, something went wrong.'
+                    return homepage_response
+
+    @staticmethod
+    def _parse_entryPoint(homepage_response):
+        try:
+            entryPoint = re.findall(
+                '"entryPoint":{"__dr":"(.*?)"}}', homepage_response.text)[0]
+        except:
+            entryPoint = 'nojs'
+        return entryPoint
+
+    @staticmethod
+    def _parse_identifier(entryPoint, homepage_response):
+        identifier = ""
+        if entryPoint in ['ProfilePlusCometLoggedOutRouteRoot.entrypoint', 'CometGroupDiscussionRoot.entrypoint']:
+            if len(re.findall('"identifier":"{0,1}([0-9]{5,})"{0,1},', homepage_response.text)) >= 1:
+                identifier = re.findall('"identifier":"{0,1}([0-9]{5,})"{0,1},', homepage_response.text)[0]
+            elif len(re.findall('fb://profile/(.*?)"', homepage_response.text)) >= 1:
+                identifier = re.findall('fb://profile/(.*?)"', homepage_response.text)[0]
+            elif len(re.findall('content="fb://group/([0-9]{1,})" />', homepage_response.text)) >= 1:
+                identifier = re.findall('content="fb://group/([0-9]{1,})" />', homepage_response.text)[0]
+        elif entryPoint in ['CometSinglePageHomeRoot.entrypoint', 'nojs']:
+            if len(re.findall('"pageID":"{0,1}([0-9]{5,})"{0,1},', homepage_response.text)) >= 1:
+                identifier = re.findall('"pageID":"{0,1}([0-9]{5,})"{0,1},', homepage_response.text)[0]
+        return identifier
 
     @staticmethod
     def _get_comments(headers: dict, post_id: str, get_post_api: str) -> requests.Response:
@@ -59,6 +105,25 @@ class Requester():
         except Exception as e:
             logger.info(f"Lỗi khi request comment {e}")
             return None 
-        
 
+    @staticmethod
+    def _get_posts(headers: dict, identifier: str, entryPoint: str, docid: str, cursor: str = "") -> requests.Response:
+        data = {
+            'variables': str({
+                'cursor': cursor,
+                'id': identifier,
+                'count': 3
+            }),
+            'doc_id': docid
+        }
+        try:
+            resp = requests.post(
+                url='https://www.facebook.com/api/graphql/',
+                data=data,
+                headers=headers
+            )
+            return resp
+        except Exception as e:
+            logger.info(f"Lỗi khi request post {e}")
+            return None
         
